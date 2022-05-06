78ec32ab3f580c1f628a9f67945d01e2dfddf05b
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-05-06 15:40:03 +0530

    create login components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 const projectID = '93448806-0b79-4664-9697-b9475e714b5e';
 
 const App = () => {
-    return (
-        <ChatEngine
-          height="100vh"
-          projectID={projectID}
-          userName="shihab@123"
-          userSecret="shihab@123"
-          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-        />
-      );
-    };
-    
-    // infinite scroll, logout, more customizations...
-    
-    export default App;
+  if (!localStorage.getItem('username')) return <LoginForm />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
